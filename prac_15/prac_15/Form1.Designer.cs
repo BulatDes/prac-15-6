22d42ea35288c910d2fcb5aa8b4d19dce685b5fe
BulatDes <123715505+BulatDes@users.noreply.github.com>
2023-05-16 01:07:51 +0500

    Add files via upload

---
+++ prac_15/prac_15/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace prac_15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.wordBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.strBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(203, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Предметный указатель";
+            // 
+            // wordBox
+            // 
+            this.wordBox.Location = new System.Drawing.Point(26, 44);
+            this.wordBox.Name = "wordBox";
+            this.wordBox.Size = new System.Drawing.Size(143, 20);
+            this.wordBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Страницы";
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Location = new System.Drawing.Point(207, 83);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(73, 20);
+            this.ButtonAdd.TabIndex = 6;
+            this.ButtonAdd.Text = "Добавить";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Слово";
+            // 
+            // strBox
+            // 
+            this.strBox.Location = new System.Drawing.Point(26, 84);
+            this.strBox.Name = "strBox";
+            this.strBox.Size = new System.Drawing.Size(143, 20);
+            this.strBox.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 36);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Вывести все слова из файла";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(26, 119);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(254, 173);
+            this.listBox1.TabIndex = 10;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(26, 363);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(106, 23);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(26, 392);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(106, 23);
+            this.searchButton.TabIndex = 12;
+            this.searchButton.Text = "Поиск";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(610, 450);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.strBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.wordBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox wordBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonAdd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox strBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ prac_15/prac_15/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_15
+{
+    public partial class Form1 : Form
+    {
+        private Subject_Index index;
+        private readonly string LoadFile = "file1.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            index = new Subject_Index();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(wordBox.Text) || string.IsNullOrEmpty(strBox.Text))
+            { MessageBox.Show("Заполните все поля.", "Внимание!"); return; }
+
+            string word = wordBox.Text.Trim();
+            string[] pageNumberStrings = strBox.Text.Split(new char[] { ',', ';' },
+                StringSplitOptions.RemoveEmptyEntries);
+            int[] pageNumbers = new int[pageNumberStrings.Length];
+            for (int i = 0; i < pageNumberStrings.Length; i++)
+            {
+                if (!int.TryParse(pageNumberStrings[i], out pageNumbers[i]))
+                {
+                    MessageBox.Show("Неверная строка чисел. Пример заполнения: '1, 2, 3, 4'.", "Ошибка.");
+                    return;
+                }
+            }
+
+            int prevNumber = int.MinValue;
+            foreach (int number in pageNumbers)
+            {
+                if (number <= prevNumber)
+                {
+                    MessageBox.Show("Числа должны быть по возрастанию и не повторяться.", "Ошибка.");
+                    return;
+                }
+                prevNumber = number;
+            }
+
+            if (pageNumbers.Length > 10)
+            {
+                MessageBox.Show("Количество номеров страниц, относящихся к одному слову," +
+                                                           " — от одного до десяти.", "Ошибка."); return;
+            }
+
+            index.AddEntry(word, pageNumbers);
+            wordBox.Text = "";
+            strBox.Text = "";
+            index.FillListBox(listBox1);
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            string word = wordBox.Text.Trim();
+            if (string.IsNullOrEmpty(word))
+            {
+                MessageBox.Show("Для удаления слова, впишите его в поле 'Слово'.",
+                "Внимание!");
+            }
+            else
+            if (!index.Contains(word))
+            {
+                MessageBox.Show($"Слово '{word}' не обнаружено в указателе. Его не удастся удалить.",
+                "Внимание!");
+            }
+            else
+            {
+                index.RemoveEntry(word);
+                index.FillListBox(listBox1);
+                MessageBox.Show($"Слово '{word}' успешно удалено.",
+               "Внимание!");
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string word = wordBox.Text.Trim();
+            if (index.Contains(word))
+            {
+                string pageNumbersString = string.Join(", ", index.GetPageNumbers(word));
+                MessageBox.Show($"Слово '{word}' имеется. Оно встречается на страницах: {pageNumbersString}",
+                    "Результат поиска.");
+            }
+            else MessageBox.Show($"Слово '{word}' не имеется.", "Результат поиска.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            index.LoadFromFile(LoadFile);
+            index.FillListBox(listBox1);
+            MessageBox.Show($"Слова были загружены из файла: '{LoadFile}'.", "Внимание!");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            index.SaveToFile(LoadFile);
+            index.FillListBox(listBox1);
+        }
+    }
+}
